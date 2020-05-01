66019b70a8e1e7758a6ef20b466e98b90972c62c
Guillaume Roux <guillaume@arras>
2020-05-01 19:09:58 +0200

    Ajout du fichier de la quatrième vidéo.

---
+++ initiation4.py
@@ -0,0 +1,19 @@
+age = 16
+condition = age >= 18
+
+if condition:
+    print("La condition est vraie.")
+    print("Instruction 1")
+    print("Instruction 2")
+else:
+    print("La condition est fausse.")
+    print("Autre instruction.")
+    
+
+n = 0 # Compteur 
+
+# Boucle "While" / Tant que
+while n <= 10:
+    print("Bonjour.")
+    print("n =",n)
+    n = n + 2 # Incrémentation du compteur
